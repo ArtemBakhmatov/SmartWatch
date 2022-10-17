6ff93647f0dcb621c20a4beea049750528236f4b
wosco <ip.bakhmatov@mail.ru>
2022-10-17 22:42:56 +0300

    Верстка и скрипты блока Header - продолжение

--- dist/js/script.js
+++ dist/js/script.js
@@ -98,14 +98,28 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "modalBuyer", function() { return modalBuyer; });
 function modalBuyer() {
     const headerTopList = document.querySelector('.header-top__list'),
-                  arrow = document.querySelector('.header-top__link-icon');
+                  arrow = document.querySelector('.header-top__link-icon'),
+             modalBuyer = document.querySelector('.header-top__modal');    
+
     headerTopList.addEventListener('click', (event) => {
         if(event.target && event.target.classList.contains('header-top__link-wrapper')) {
             if(!arrow.classList.contains('header-top__link-open')) {
                 arrow.classList.add('header-top__link-open');
+                modalBuyer.classList.add('header-top__modal-active');
             } else {
                 arrow.classList.remove('header-top__link-open');
+                modalBuyer.classList.remove('header-top__modal-active');
             }
+        }
+    });
+
+    arrow.addEventListener('click', () => {
+        if(!arrow.classList.contains('header-top__link-open')) {
+            arrow.classList.add('header-top__link-open');
+            modalBuyer.classList.add('header-top__modal-active');
+        } else {
+            arrow.classList.remove('header-top__link-open');
+            modalBuyer.classList.remove('header-top__modal-active');
         }
     });
 }
--- src/js/modules/modalBuyer.js
+++ src/js/modules/modalBuyer.js
@@ -1,13 +1,27 @@
 function modalBuyer() {
     const headerTopList = document.querySelector('.header-top__list'),
-                  arrow = document.querySelector('.header-top__link-icon');
+                  arrow = document.querySelector('.header-top__link-icon'),
+             modalBuyer = document.querySelector('.header-top__modal');    
+
     headerTopList.addEventListener('click', (event) => {
         if(event.target && event.target.classList.contains('header-top__link-wrapper')) {
             if(!arrow.classList.contains('header-top__link-open')) {
                 arrow.classList.add('header-top__link-open');
+                modalBuyer.classList.add('header-top__modal-active');
             } else {
                 arrow.classList.remove('header-top__link-open');
+                modalBuyer.classList.remove('header-top__modal-active');
             }
+        }
+    });
+
+    arrow.addEventListener('click', () => {
+        if(!arrow.classList.contains('header-top__link-open')) {
+            arrow.classList.add('header-top__link-open');
+            modalBuyer.classList.add('header-top__modal-active');
+        } else {
+            arrow.classList.remove('header-top__link-open');
+            modalBuyer.classList.remove('header-top__modal-active');
         }
     });
 }
