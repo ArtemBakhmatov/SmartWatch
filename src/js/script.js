c3db797a56a27e19a3499477e7333bfc3d548dd2
wosco <ip.bakhmatov@mail.ru>
2022-11-13 15:51:23 +0300

    Карточки товара JS детские

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ import { modalBuyer } from "./modules/modalBuyer";
 import { offerSlider } from "./modules/offerSlider";
 import { offerTimer } from "./modules/offerTimer";
 import { reviewsSlider } from "./modules/reviewsSlider";
-import { cardSliderColor } from "./modules/cardSliderColor";
+import { menuCardChildren } from "./modules/menuCardChildren";
 
 window.addEventListener('DOMContentLoaded', () => {
     modalCatalog();
@@ -11,5 +11,5 @@ window.addEventListener('DOMContentLoaded', () => {
     offerSlider();
     offerTimer();
     reviewsSlider();
-    cardSliderColor();
+    menuCardChildren();
 });
